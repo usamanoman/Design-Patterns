0612d1f51dc31d25b40839d2a16510577b48d0ad
Usama Noman <usama.noman@outlook.com>
2014-09-01 07:51:40 +0500

    Observer Pattern Wrong and Right Examples

---
+++ ObserverPatternImplementationWrongExample/ObserverPatternImplementation/WeatherData.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObserverPatternImplementation
+{
+
+    class currentCondition
+    {
+        public void display(int temp, int press, int hum)
+        {
+
+        }
+    }
+
+    class forecastCondition
+    {
+        public void display(int temp, int press, int hum)
+        {
+
+        }
+    }
+
+    class statistis
+    {
+        public void display(int temp, int press, int hum)
+        {
+
+        }
+    }
+
+
+
+    class WeatherData
+    {
+        public int temperature;
+        public int humidity;
+        public int pressure;
+
+        
+
+        public void measurementsChanged()
+        {
+            this.temperature = getTemparature();
+            this.humidity = getHumidity();
+            this.pressure = getPressure();
+            currentCondition currentCond = new currentCondition();
+            forecastCondition forecast = new forecastCondition();
+            statistis statistical = new statistis();
+
+            currentCond.display(temperature,  pressure,humidity);
+            statistical.display(temperature, pressure, humidity);
+            forecast.display(temperature, pressure, humidity);
+
+
+
+
+        }
+    }
+}
---
+++ ObserverPatternRightExample/ObserverPatternRightExample/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObserverPatternRightExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WeatherData wd = new WeatherData();
+            Forecast fc = new Forecast(wd);
+            wd.setMeasurements((float)10.3, (float)23.4, (float)98.0);
+
+        }
+    }
+}
