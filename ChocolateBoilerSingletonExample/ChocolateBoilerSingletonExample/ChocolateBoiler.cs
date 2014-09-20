10df55b849946830bb54babe7d52f37654c88463
Usama Noman <k112119@nu.edu.pk>
2014-09-20 17:45:52 +0500

    ChocolateBoilerSingletonExample

---
+++ ChocolateBoilerSingletonExample/ChocolateBoilerSingletonExample/ChocolateBoiler.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChocolateBoilerSingletonExample
+{
+    public class ChocolateBoiler
+    {
+        public bool empty { get; set; }
+        public bool boil { get; set; }
+        private static ChocolateBoiler cber;
+        private  ChocolateBoiler()
+        {
+            this.empty = true;
+            this.boil = true;
+
+        }
+        public static ChocolateBoiler getInstance()
+        {
+            if (cber == null)
+            {
+                cber = new ChocolateBoiler();
+            }
+            return cber;
+        }
+        public void fill()
+        {
+            if (this.empty == true)
+            {
+                this.empty = false;
+                this.boil = false;
+                Console.WriteLine("The boiler is full");
+
+            }
+        }
+
+        public void drain()
+        {
+            if (this.empty == false && this.boil == true)
+            {
+                this.empty = true;
+                this.boil = true;
+                Console.WriteLine("The boiler is drain");
+            }
+        }
+        public void boilMethod()
+        {
+            if (this.boil == false && this.empty==false)
+            {
+                this.boil = true;
+                Console.WriteLine("The boiler is boiled");
+
+            }
+        }
+    }
+}
---
+++ ChocolateBoilerSingletonExample/ChocolateBoilerSingletonExample/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChocolateBoilerSingletonExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChocolateBoiler cb = ChocolateBoiler.getInstance();
+            cb.fill();
+            cb.boilMethod();
+            //cb.drain();
+            ChocolateBoiler bc = ChocolateBoiler.getInstance();
+            bc.fill();
+        }
+    }
+}
